4ae25b49bc441940bdbe21a4b641324becc5ea3c
DIR\t.krittayanurak <t.krittayanurak@accenture.com>
2021-07-28 18:49:58 +0700

    first commit

---
+++ aboutTK/home/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('', Home, name='home-page'),
+    path('service/', Service, name='service'),
+    path('contact/', Contact, name='contact'),
+]
